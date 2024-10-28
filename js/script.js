c8deea55bb440d50d242f827a00002e670ecb4e4
helloprof <suluxan.seneca@gmail.com>
2024-10-28 17:01:45 -0400

    adding cards on click (create the card node, apply the card's class, then appendChild the card to the parent element)

--- js/script.js
+++ js/script.js
@@ -26,3 +26,51 @@ sidebar.style.backgroundColor = "green"
 
 nav.appendChild(sidebar)
 
+
+let button = document.createElement("button")
+button.innerText = "Click Me"
+button.style.backgroundColor = "white"
+
+
+button.addEventListener("click", function(){
+    let newCard = document.createElement("div")
+    let cardImg = document.createElement("img")
+    cardImg.src = "https://upload.wikimedia.org/wikipedia/en/f/ff/Heart_on_My_Sleeve_ghostwriter977.jpg"
+    cardImg.width = "500px"
+
+    let cardTitle = document.createElement("h3")
+    cardTitle.textContent = "Ghostwriter"
+
+    let audioPlay = document.createElement("audio")
+    audioPlay.controls = true
+    audioPlay.src = "https://ia801606.us.archive.org/30/items/ghostwriter-heart-on-my-sleeve-ai-xenen-mix-master-1080p-mux/ghostwriter%20heart%20on%20my%20sleeve%20AI%20XENEN%20Mix%20Master_1080p_MUX.mp4"
+
+
+    newCard.appendChild(cardImg)
+    newCard.appendChild(cardTitle)
+    newCard.appendChild(audioPlay)
+
+    newCard.className = "card-div"
+
+    let homepage = document.querySelector("#homepage")
+    homepage.appendChild(newCard)
+
+})
+
+let homepage = document.querySelector("#homepage")
+
+homepage.appendChild(button)
+
+
+
+
+
+{/* <div id="ghostwriter" class="card-div">
+    <img width="500px" src="https://upload.wikimedia.org/wikipedia/en/f/ff/Heart_on_My_Sleeve_ghostwriter977.jpg"> 
+        ghostwriter
+    <div>
+        <audio controls autoplay> 
+            <source src="https://ia801606.us.archive.org/30/items/ghostwriter-heart-on-my-sleeve-ai-xenen-mix-master-1080p-mux/ghostwriter%20heart%20on%20my%20sleeve%20AI%20XENEN%20Mix%20Master_1080p_MUX.mp4" type="audio/mpeg">
+        </audio>  
+    </div>
+</div> */}
