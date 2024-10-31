e1b95aeb7de81a6e47b5da82027ed3aa59cbf35d
helloprof <suluxan.seneca@gmail.com>
2024-10-31 14:58:25 -0400

    adding new ai music data

---
+++ js/artists.js
@@ -0,0 +1,16 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  {
+    artistID: 1,
+    name: "ghostwrite977"
+  },
+  {
+    artistID: 2,
+    name: "genericAI123"
+  }
+];
---
+++ js/songs.js
@@ -0,0 +1,33 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  // {
+  //   songId: “SID-13423453”,
+  //   artistId: “AID-51234”,
+  //   title: “Song Title”,
+  //   album: “Album Name”
+  //   year: “2020”,
+  //   duration: 196,
+  //   url: “https://www.youtube.com/watch?v=dQw4w9WgXcQ”,
+  //   explicit: true
+  // }
+  {
+    songID: 1,
+    artistID: 1,
+    title: "Heart on my Sleeve",
+    year: 2023,
+    explicit: true
+  },
+  {
+    songID: 2,
+    artistID: 2,
+    title: "Ghostwriter",
+    year: 2023,
+    explicit: true
+  },
+  
+];
