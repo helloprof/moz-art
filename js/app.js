c2d9e38b411cf1e89b011ee578c967ca50cdda73
helloprof <suluxan.seneca@gmail.com>
2024-11-04 17:02:46 -0500

    modularizing/separating functions to avoid 1 nested function, also deleting the existing songs to make new element to hold songs on click

--- js/app.js
+++ js/app.js
@@ -20,28 +20,51 @@ const { artists, songs } = window;
 // For debugging, display all of our data in the console. You can remove this later.
 console.log({ artists, songs }, "App Data");
 
+function generateSongsList(artistSong) {
+    let songsDivNew = document.querySelector("#songs")
+    let songP = document.createElement("p") // table rows here
+    songP.innerText = artistSong.title
+
+    songsDivNew.appendChild(songP)
+}
+
 function generateArtistList() {
     let homepage = document.querySelector("#homepage")
     artists.forEach((artist) => {
         let artistButton = document.createElement("button")
+        let icon = document.createElement("i")
+        icon.className = "fas fa-user"
+        
         artistButton.innerText = artist.name
         artistButton.style.backgroundColor = "green"
-        artistButton.style.borderRadius = "50%"
+        artistButton.style.borderRadius = "30px"
         artistButton.style.margin = "10px"
         artistButton.style.padding = "10px"
+        artistButton.appendChild(icon)
+
 
         homepage.appendChild(artistButton)
 
         artistButton.addEventListener("click", function(){
+
+            // check if existing songs are displayed
+            let songsDiv = document.querySelector("#songs")
+            if(songsDiv){
+                songsDiv.remove()  // table body removed (existing songs)
+            }
+            
+            const songsDivNew = document.createElement("div") // table body recreated (new one created to hold the new artist songs)
+            songsDivNew.id = "songs"
+            songsDivNew.style.backgroundColor = "black"
+
+            homepage.appendChild(songsDivNew)
+
             let artistSongs = songs.filter((song) => {
                 return song.artistID === artist.artistID
             })
-
+            
             artistSongs.forEach((artistSong) => {
-                let songP = document.createElement("p")
-                songP.innerText = artistSong.title
-
-                homepage.appendChild(songP)
+                generateSongsList(artistSong)
             })
 
 
