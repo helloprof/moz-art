c26c4a6b89bcef6161fa4e443ee01034c45782f7
helloprof <suluxan.seneca@gmail.com>
2024-11-18 17:08:38 -0500

    new songs (update album covers, will eventually generate these)

--- js/songs.js
+++ js/songs.js
@@ -20,27 +20,132 @@ window.songs = [
     artistID: 1,
     title: "Heart on my Sleeve",
     year: 2023,
-    explicit: true
+    explicit: true,
+    albumCover: "/images/ghostwriter.jpg", // change image
+    audioMP4: "https://ia601606.us.archive.org/30/items/ghostwriter-heart-on-my-sleeve-ai-xenen-mix-master-1080p-mux/ghostwriter%20heart%20on%20my%20sleeve%20AI%20XENEN%20Mix%20Master_1080p_MUX.ia.mp4"
   },
   {
     songID: 2,
     artistID: 2,
-    title: "Ghostwriter",
+    title: "Forever",
     year: 2023,
-    explicit: true
+    explicit: true,
+    albumCover: "/images/corwii.webp", // locate album image
+    audioMP4: "https://cdn1.suno.ai/75f42771-2e4a-493c-831f-3bbed57eb0e4.mp3"
   },
   {
     songID: 3,
-    artistID: 1,
-    title: "Heart on my Sleeve",
+    artistID: 3,
+    title: "Gold Hands",
     year: 2023,
-    explicit: true
+    explicit: true,
+    albumCover: "/images/unnamed.png", // locate album image
+    audioMP4: "https://storage.googleapis.com/udio-artifacts-c33fe3ba-3ffe-471f-92c8-5dfef90b3ea3/samples/41307c7723d246599a7891b9287157e0/c2e950fb-5ae4-4f65-b39c-fb5bf8600661/1.mp4",
   },
   {
     songID: 4,
-    artistID: 2,
-    title: "Ghostwriter",
+    artistID: 4,
+    title: "Push it down",
     year: 2023,
-    explicit: true
+    explicit: true,
+    albumCover: "/images/halo.jpg", // locate album image
+    audioMP4: null
+  },
+  {
+    songID: 5,
+    artistID: 1,
+    title: "Heart on my Sleeve",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/ghostwriter.jpg", // change image
+    audioMP4: "https://ia601606.us.archive.org/30/items/ghostwriter-heart-on-my-sleeve-ai-xenen-mix-master-1080p-mux/ghostwriter%20heart%20on%20my%20sleeve%20AI%20XENEN%20Mix%20Master_1080p_MUX.ia.mp4"
+  },
+  {
+    songID: 6,
+    artistID: 2,
+    title: "Forever",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/corwii.webp", // locate album image
+  },
+  {
+    songID: 7,
+    artistID: 3,
+    title: "Gold Hands",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/unnamed.png", // locate album image
+  },
+  {
+    songID: 8,
+    artistID: 4,
+    title: "Push it down",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/halo.jpg", // locate album image
+  },
+  {
+    songID: 9,
+    artistID: 1,
+    title: "Heart on my Sleeve",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/ghostwriter.jpg", // change image
+  },
+  {
+    songID: 10,
+    artistID: 2,
+    title: "Forever",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/corwii.webp", // locate album image
+  },
+  {
+    songID: 11,
+    artistID: 3,
+    title: "Gold Hands",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/unnamed.png", // locate album image
+  },
+  {
+    songID: 12,
+    artistID: 4,
+    title: "Push it down",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/halo.jpg", // locate album image
+  },
+  {
+    songID: 13,
+    artistID: 1,
+    title: "Heart on my Sleeve",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/ghostwriter.jpg", // change image
+  },
+  {
+    songID: 14,
+    artistID: 2,
+    title: "Forever",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/corwii.webp", // locate album image
+  },
+  {
+    songID: 15,
+    artistID: 3,
+    title: "Gold Hands",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/unnamed.png", // locate album image
+  },
+  {
+    songID: 16,
+    artistID: 4,
+    title: "Push it down",
+    year: 2023,
+    explicit: true,
+    albumCover: "/images/halo.jpg", // locate album image
   },
 ];
