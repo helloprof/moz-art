47943606f5af230ec25e3b07efb43c6677a3b334
helloprof <suluxan.seneca@gmail.com>
2024-11-18 17:08:21 -0500

    new artist data

--- js/artists.js
+++ js/artists.js
@@ -2,15 +2,56 @@
  * artists.js
  *
  * The app's list of Artists
+ * 
+ * 
+- Heart on my sleeve
+    - ghostwriter977
+    - April 2023
+    - Drake/Weeknd
+- Trappin 4 the amish 
+    - Banned records 
+    - May 6 2024
+    - new
+- Push it down 
+    - H.A.L.O.
+    - May 12 2024
+    - Kid cudi? 
+- Forever 
+    - Corwii
+    - May 1 2024
+    - Post malone?
+- Gold Hands
+    - Alabasta 
+    - Future
+    - May 18 2024
  */
 
+
+// 
 window.artists = [
   {
     artistID: 1,
-    name: "ghostwrite977"
+    name: "ghostwriter977",
+    profilePic: "/images/ghostwriter.jpg",
   },
   {
     artistID: 2,
-    name: "genericAI123"
-  }
+    name: "Corwii",
+    profilePic: "/images/corwii.webp",
+  },
+  {
+    artistID: 3,
+    name: "Alabasta",
+    profilePic: "/images/unnamed.png",
+  },
+  {
+    artistID: 4,
+    name: "H.A.L.O.",
+    profilePic: "/images/halo.jpg",
+  },
+  // {
+  //   artistID: 5,
+  //   name: "Banned Records",
+  //   profilePic: "/images/bannedrecords.jpg",
+  // }
 ];
